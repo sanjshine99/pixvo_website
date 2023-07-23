edf699b55118dcf0c4f6021afb5d57b7882c9e21
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-07-22 22:25:53 -0700

    main template

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,33 +6,33 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer_main">
-      <div className="footer__nav">
-        <div className="footer__left">
-          <ul className="footer__section">
-            <li ><Link to="/about" className='footer_title'>About Us</Link></li>
-            <li className='footer_item'><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
-            <li className='footer_item'><a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-          </ul>
+        <div className="footer__nav">
+          <div className="footer__left">
+            <ul className="footer__section">
+              <li><Link to="/about" className='footer_title'>About Us</Link></li>
+              <li className='footer_item'><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+              <li className='footer_item'><a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+            </ul>
+          </div>
+          <div className="footer__right">
+            <ul className="footer__section">
+              <li><Link to="/project" className='footer_title'>Projects</Link></li>
+              <li className='footer_item'><a href="/blog">Blog</a></li>
+              <li className='footer_item'><a href="/portfolio">Portfolio</a></li>
+            </ul>
+          </div>
+          <div className="footer__section">
+            <ul className="footer__section">
+              <li><Link to="/contact" className='footer_title'>Contact</Link></li>
+              <li className='footer_item'>Email: contact@pixvo.studio</li>
+              <li className='footer_item'>Mobile: +1 123-456-7890</li>
+            </ul>
+          </div>
         </div>
-        <div className="footer__right">
-          <ul className="footer__section">
-          <li ><Link to="/project" className='footer_title'>Projects</Link></li>
-            <li className='footer_item'><a href="/blog">Blog</a></li>
-            <li className='footer_item'><a href="/portfolio">Portfolio</a></li>
-          </ul>
-        </div>
-        <div className="footer__right">
-        <ul className="footer__section">
-          <li ><Link to="/contact" className='footer_title'>Contact</Link></li>
-            <li className='footer_item'>Email: contact@pixvo.studio</li>
-            <li className='footer_item'>Mobile: +1 123-456-7890</li>
-          </ul>
-        </div>
+        <hr className="footer__bar" />
+        <p className="footer__copy">&copy; 2023 Pixvo.Studio</p>
       </div>
-      <hr className="footer__bar" />
-      <p className="footer__copy">&copy; 2023 Pixvo.Studio</p>
-    </div>
- </footer>
+    </footer>
   );
 }
 
