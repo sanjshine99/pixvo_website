e1c0a54e729fb8464a9835dc86a8658c9d2092c1
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-07-22 22:21:27 -0700

    main template

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,17 +5,13 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <footer className="footer">
+      <div className="footer_main">
       <div className="footer__nav">
         <div className="footer__left">
           <ul className="footer__section">
             <li ><Link to="/about" className='footer_title'>About Us</Link></li>
             <li className='footer_item'><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
             <li className='footer_item'><a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-          </ul>
-          <ul className="footer__section">
-          <li ><Link to="/contact" className='footer_title'>Contact</Link></li>
-            <li className='footer_item'>Email: contact@pixvo.studio</li>
-            <li className='footer_item'>Mobile: +1 123-456-7890</li>
           </ul>
         </div>
         <div className="footer__right">
@@ -25,10 +21,18 @@ function Footer() {
             <li className='footer_item'><a href="/portfolio">Portfolio</a></li>
           </ul>
         </div>
+        <div className="footer__right">
+        <ul className="footer__section">
+          <li ><Link to="/contact" className='footer_title'>Contact</Link></li>
+            <li className='footer_item'>Email: contact@pixvo.studio</li>
+            <li className='footer_item'>Mobile: +1 123-456-7890</li>
+          </ul>
+        </div>
       </div>
       <hr className="footer__bar" />
       <p className="footer__copy">&copy; 2023 Pixvo.Studio</p>
-    </footer>
+    </div>
+ </footer>
   );
 }
 
